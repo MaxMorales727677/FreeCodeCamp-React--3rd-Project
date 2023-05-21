6dc55503b6efd404dfd69772c6532a04dc46784c
MaxMorales727677 <jesus.morales.pintor@u-erre.mx>
2023-05-20 22:56:42 -0600

    Creación de plantilla inicial de Aplicacion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import freeCodeCampoLogo from './imagenes/FreeCodeCamp_logo.png';
 
 function App() {
   return (
     <div className="App">
-      Hola haha
+      <div className='freecodecamp-logo-contenedor'> 
+        <img className='freecodecamp-logo'
+          src={freeCodeCampoLogo}
+          alt='Esta es una imagen muajaja'
+        />
+      </div> 
+      <div className='contenedor-calculadora'> 
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+        <div className='fila'> </div>
+      </div>
     </div>
   );
 }
